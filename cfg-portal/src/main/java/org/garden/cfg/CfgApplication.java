f76f127417a3bd5a6e9a74e790b341dc5dd65462
goaler <18395587850@163.com>
2018-07-31 22:21:08 +0800

    构建多项目

---
+++ cfg-portal/src/main/java/org/garden/cfg/CfgApplication.java
@@ -0,0 +1,12 @@
+package org.garden.cfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfgApplication {
+
+	public static void main(String[] args) throws InterruptedException {
+		SpringApplication.run(CfgApplication.class, args);
+	}
+}
